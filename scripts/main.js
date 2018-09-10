1bb66142ec05a03d517616e2ac84522dd944c60f
stevedoesitall <stevegiordano@MBA0395.local>
2018-09-10 18:13:04 -0400

    initial push

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+import {cl, headers} from "https://rawgit.com/stevedoesitall/ditkojs/master/ditko.js";
+
+
+function get_intel() {
+    cl('hi')
+    fetch("http://feed.sailthru.com/ws/feed?id=57291f1b1aa312342f8b456b", {
+        method: "post",
+        headers: headers,
+        mode: "cors"
+    })
+
+    .then(
+        function(response) {
+            if (response.status != 200) {
+                cl("Error: " + response.status);
+                throw new Error ("Something went wrong. Please try again later.");
+            }
+
+            response.json().then(
+                function(resp_data) {
+                    cl(resp_data);
+                }
+            )
+        }
+    )
+    .catch((err) => {
+        alert(err);
+    });
+}
+
+window.onload = get_intel();
---
+++ scripts/server.js
@@ -0,0 +1,14 @@
+//Create app using express.js
+const port = process.env.PORT || 3000;
+const express = require("express");
+const body_parser = require("body-parser");
+const path = require("path");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const dir = path.join(__dirname, "../");
+
+app.use(express.static(dir));
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
+app.listen(port, () => console.log("Competitor feed app started on port " + port));
